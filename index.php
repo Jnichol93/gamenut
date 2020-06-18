b48c93a6ffd434265a3a046a615b4468112d1307
jnichol93 <478643@students.gateshead.ac.uk>
2020-06-18 02:02:00 +0100

    first commit

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <title> BarelySurvivedGaming </title>
 <head>
 
-<link href="landing.css" type="text/css" rel="stylesheet">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link  rel ="stylesheet" href="landing.css" type="text/css" rel="stylesheet">
+
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta charset = "utf-8">
 
@@ -17,41 +17,20 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<style>
-body {
-  background-image: url(starry.jpg);
-}
+
+
 </style>
 </head>
 
 
 <body>
 
+            <div class = container>
+                  <div class = titlebar>
 
 
-
-
-  <div class = "title"><div class = "bsg"> <h1> BSGaming</h1></div>
-
-
-
-  <div class= "bar"> <marquee scrollamount = "15"> The Ultimate Gaming Hub | Chat With Fellow Gamers | Promote Your Content |
-    Get The Latest News | See Upcoming Games | Review Your Favourite Games |
-    Listen to Podcasts | Browse For Merchandise | And Much More! | </marquee> </div></div>
-
-  <div class = "cont">
-
-<a href = "home.html"><div class = page>  <img src ="home.png">  </div> </a>
-<div class = page1><img src ="podcasts.png"> </div>
-<div class = page2><img src ="platforms.png"></div>
-<div class = page3><img src ="esports.png"></div>
-<div class = page4><img src ="forum.png"></div>
-<div class = page5><img src ="hwex.png"></div>
-<div class = page6><img src ="vids.png"></div>
-<div class = page7><img src ="merch.png"></div>
-
-
-</div>
+            </div>
+  </div>
 
 
 
